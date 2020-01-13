62f7bca2a6ca9dc313e828ae9965c26ee8f282a3
Ian Wootten <hi@niftydigits.com>
2020-01-13 11:22:41 +0000

    Backtrack changes to test other language codes

--- scripts/pull_and_run_validator.sh
+++ scripts/pull_and_run_validator.sh
@@ -2,5 +2,5 @@
 
 branch=v3
 docker pull onsdigital/eq-schema-validator:$branch
-$(docker run -d -p 5001:5000 "onsdigital/eq-schema-validator:$branch")
+docker run -d -p 5001:5000 "onsdigital/eq-schema-validator:$branch"
 sleep 3
