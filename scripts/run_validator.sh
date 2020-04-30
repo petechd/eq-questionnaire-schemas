90393208bc94574488b3244a14722d3b2ec4faa1
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-04-30 15:12:36 +0100

    Temporarily revert validator branch (#53)

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-branch=latest
-docker pull onsdigital/eq-questionnaire-validator:$branch
-docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
+# https://github.com/ONSdigital/eq-questionnaire-validator/commit/ef9e236d8428a5661f7e7501323cc29bcf313e4c
+tag=allow-multiple-list-collectors
+docker pull onsdigital/eq-questionnaire-validator:$tag
+docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
