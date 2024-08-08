6def35eb119c61172657503235fd721066f6547f
petechd <53475968+petechd@users.noreply.github.com>
2024-08-08 14:00:05 +0100

    Use v2 version of docker compose command (#523)

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 tag=`cat .schema-validator-version`
-TAG=${tag} docker-compose -f docker-compose-schema-validator.yml up -d
+TAG=${tag} docker compose -f docker-compose-schema-validator.yml up -d
